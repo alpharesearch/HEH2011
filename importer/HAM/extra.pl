5c843b792a21be93765737818224f5d6f3835999
Markus Schulz <schulz@alpharesearch.de>
2011-07-14 19:15:42 -0400

    make pool more machine readable

---
+++ importer/HAM/extra.pl
@@ -0,0 +1,57 @@
+#perl -w
+#start at 12:48
+ #
+ #HamExamHelper, the little helper to help you pass your ham exams!
+ #Copyright (C) 2006 Markus Schulz
+ # 
+ #This program is free software; you can redistribute it and/or
+ #modify it under the terms of the GNU General Public License
+ #as published by the Free Software Foundation; either version 2
+ #of the License, or (at your option) any later version.
+ #
+ #This program is distributed in the hope that it will be useful,
+ #but WITHOUT ANY WARRANTY; without even the implied warranty of
+ #MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ #GNU General Public License for more details.
+ #
+ #You should have received a copy of the GNU General Public License
+ #along with this program; if not, write to the Free Software
+ #Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ #
+ #
+ # Created by Markus Schulz with the help of SharpDevelop and Visual C# express.
+ # User: $Author$
+ # Date: $LastChangedDate$
+ # Rev : $Rev$
+ # 
+ # ID: $Id$
+ #
+use strict;
+open PINPUT,"<Element 4 Pool.txt";
+open POUTPUT,">Element 4 Pool_C.txt";
+while(<PINPUT>)
+	{
+		chomp;
+		if (/~~/){print 'skip';}
+		else{
+			if (/(((E)\d[A-Z]\d))/) {
+			print POUTPUT "~~~\n$`$&$'\n";
+		    }
+		    else {
+				 if (/(((E)\d[A-Z]))/) {
+				 print POUTPUT "~~\n$`$&$'\n";
+				 }
+				else{
+					if (/((SUBELEMENT))/) {
+					print POUTPUT "~\n$`$&$'\n";
+					}
+					else{
+						print POUTPUT $_."\n";
+					}
+				}
+		    }
+		}
+	}
+print POUTPUT "~~\n";
+close POUTPUT;
+close PINPUT;
